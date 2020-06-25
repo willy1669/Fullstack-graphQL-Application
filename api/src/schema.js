3cd37175ecf55fa29bd7d8b4cbfeef65c2552dc7
chinonso Wiliiams <williamschinonso11gmail.com>
2020-06-25 13:14:40 +0100

    create a mutation for new shoe

--- api/src/schema.js
+++ api/src/schema.js
@@ -1,45 +1,53 @@
-const { gql } = require('apollo-server')
+const { gql } = require("apollo-server");
 
 /**
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
   type User {
-      id: ID!
-      userame: String!
-      name: String!
-      firstName: String!
-      lastName: String!
+    id: ID!
+    userame: String!
+    name: String!
+    firstName: String!
+    lastName: String!
   }
 
   type Shoe {
-      size: Int!
-      brand: String!
+    size: Int!
+    brand: String!
   }
 
   input ShoeInput {
-      brand: String
-      size: Int
+    brand: String
+    size: Int
+  }
+
+  input NewShoeInput {
+    brand: String!
+    size: Int!
   }
 
   type Pet {
-      id: ID!
-      createdAt: String!
-      name: String!
-      type: String!
+    id: ID!
+    createdAt: String!
+    name: String!
+    type: String!
   }
 
   input PetInput {
-      name: String
-      type: String
+    name: String
+    type: String
   }
 
   type Query {
-      shoes(input: ShoeInput): [Shoe]!
-      pets(input: PetInput): [Pet]!
-      pet(input: PetInput): Pet
+    shoes(input: ShoeInput): [Shoe]!
+    pets(input: PetInput): [Pet]!
+    pet(input: PetInput): Pet
   }
 
+  type Mutation {
+      newShoe(input: NewShoeInput!): Shoe!
+  }
 `;
 
-module.exports = typeDefs
+module.exports = typeDefs;
